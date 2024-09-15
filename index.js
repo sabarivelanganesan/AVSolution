d694248552ed0344d1fec1eeae9c568478f11103
Sabarivelan Ganesan <sabarivelangn@gmail.com>
2024-09-15 02:04:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function formSubmission() {
+    alert("Thank you for reaching out. Your details have been received by our team, and we will be in touch with you shortly")
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const observerOptions = {
+        threshold: 0.1 // Trigger when 10% of the element is visible
+    };
+
+    const fadeInElementsLeft = document.querySelectorAll('.card-left');
+    const fadeInElementsRight = document.querySelectorAll('.card-image');
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains('card-left')) {
+                    entry.target.classList.add('fade-slide-left');
+                } else if (entry.target.classList.contains('card-image')) {
+                    entry.target.classList.add('fade-slide-right');
+                }
+            }
+        });
+    }, observerOptions);
+
+    fadeInElementsLeft.forEach(element => {
+        observer.observe(element);
+    });
+
+    fadeInElementsRight.forEach(element => {
+        observer.observe(element);
+    });
+});
